80d1b465f3a6f7c7c03c378fd63e8b0e833deaaa
romanshalykhin <roman.shalyhin@gmail.com>
2017-04-07 23:17:09 +0300

    Added some files

---
+++ 26.php
@@ -0,0 +1,10 @@
+<?php
+/*26. Объявите   константу DAYS_COUNT равную 7 и константу MONTH_COUNT равную 12 двумя разными способами объявления констант.*/
+
+// способ 1:
+difine(DAYS_COUNT, 7);
+difine(MONTH_COUNT, 12);
+
+//способ 2:
+const DAYS_COUNT = 7;
+const MONTH_COUNT = 12;
